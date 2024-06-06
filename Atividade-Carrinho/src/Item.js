13baafddcb29e21e4db1a7727e0954971ce22053
JorbeJr <50496289+Jorbee1234@users.noreply.github.com>
2024-06-05 21:49:50 -0300

    Atividade para relembrar alguns conceitos de react
    Estudando front-end para procurar uma biblioteca que eu goste, tire um tempo para analisar react

---
+++ Atividade-Carrinho/src/Item.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Item.css';
+
+const Item = ({ item, onAddToCart, children }) => {
+    return (
+        <div className="Item">
+            <div className="Item-left">
+                <div className="Item-image">
+                    {/* Add image content here */}
+                </div>
+                <div className="Item-title">
+                    {item.name}
+                </div>
+                <div className="Item-description">
+                    {item.description}
+                </div>
+            </div>
+
+            <div className="Item-right">
+                <div className="Item-price">
+                    ${item.price}
+                </div>
+                {children}
+            </div>
+        </div>
+    );
+};
+
+Item.propTypes = {
+    item: PropTypes.object.isRequired,
+    onAddToCart: PropTypes.func,
+    children: PropTypes.node
+};
+
+export default Item;
---
+++ Atividade-Carrinho/src/static-data.js
@@ -0,0 +1,22 @@
+let items = [
+    {
+        id: 0, 
+        name:"Notebook dell inspiron", 
+        description:"Excelente para programar", 
+        price: 3000
+    },
+    {
+        id: 1, 
+        name:"Celular S24", 
+        description:"Excelente Celular", 
+        price: 4000
+    },
+    {
+        id: 2, 
+        name:"Teclado gamer", 
+        description:"Excelente para programar e jogar", 
+        price: 500
+    }
+];
+
+export {items}
